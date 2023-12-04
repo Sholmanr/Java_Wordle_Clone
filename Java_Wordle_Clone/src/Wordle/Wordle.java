6f6c0375fd770be74cd454bd6428990fdf5fd644
SHolmanr <simjaehr@gmail.com>
2023-12-03 23:03:23 -0500

    User input added

--- Java_Wordle_Clone/src/Wordle/Wordle.java
+++ Java_Wordle_Clone/src/Wordle/Wordle.java
@@ -5,11 +5,35 @@ public class Wordle {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		String answer;
+		
 		Scanner scnr = new Scanner(System.in); 
 		
-		System.out.print("Do you want to play");
+		// Gets the users input and determines whether to start the game
+		System.out.print("Do you want to play? y/n\n");
+		answer = scnr.next();
 		
+		if(answer.compareTo("y") == 0)
+		{
+			play();
+			scnr.close();
+		}
+		else if(answer.compareTo("n") == 0)
+		{
+			System.out.println("Goodbye!");
+		}
+		else
+		{
+			System.out.print("Invalid Response");
+		}
 
 	}
+	
+	
+	public static void play() 
+	{
+		setWord word = new setWord(); 
+	}
+
 
 }
--- Java_Wordle_Clone/src/Wordle/setWord.java
+++ Java_Wordle_Clone/src/Wordle/setWord.java
@@ -1,5 +1,9 @@
 package Wordle;
 
-public class setWord {
+import java.io.*;
 
+public class setWord {
+	
+	File newFile = new File("Wordle_Words");
+	
 }
