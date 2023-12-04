ff66391289e3d6249a20d3d0360f7ecbba56c4ed
SHolmanr <simjaehr@gmail.com>
2023-12-03 23:24:47 -0500

    Initial Commit

---
+++ Wordle/src/wordle/main.java
@@ -0,0 +1,16 @@
+package wordle;
+
+import java.util.Scanner; 
+import java.io.*;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String answer; 
+		
+		Scanner scnr = new Scanner(System.in);
+		
+	}
+
+}
